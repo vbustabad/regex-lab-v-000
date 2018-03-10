78d0eca4cc6990399ba4db3e012f3e417ed2be17
Veronica Bustabad <vbustabad@gmail.com>
2018-03-10 00:56:24 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,17 @@
 def starts_with_a_vowel?(word)
-
+  if word.scan(/\b\s˄[AEIOUaeiou]\s\b\w+/)
+    return true
+  elsif word.scan(/\b\s[A-Za-z&&[˄AEIOUaeiou]]\s\b/)
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\b[˄un][a-z][ing$]\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
