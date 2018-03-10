a96dbccb04aa72f6406dbee4c7f881b0d7fa4cac
Veronica Bustabad <vbustabad@gmail.com>
2018-03-10 01:33:43 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.match(/˄[aeiouAEIOU]\w+/)
-    return true
-  else
-    return false
-  end
+  word.match(/˄[aeiouAEIOU]\w+/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
